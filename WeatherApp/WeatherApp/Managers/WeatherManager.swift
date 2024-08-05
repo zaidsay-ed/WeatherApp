226d033e11195cc38faa6f1a0a182097f7be2575
zsayed <zsayed@student.ubc.ca>
2024-08-05 20:25:14 +0400

    JSON example

--- WeatherApp/WeatherApp/Managers/WeatherManager.swift
+++ WeatherApp/WeatherApp/Managers/WeatherManager.swift
@@ -25,5 +25,44 @@ class WeatherManager {
 }
 
 struct ResponseBody:Decodable {
+    var coord: CoordinatesResponse
+    var weather: [WeatherResponse]
+    var main: MainResponse
+    var name: String
+    var wind: WindResponse
     
+    struct CoordinatesResponse: Decodable{
+        var lon: Double
+        var lat: Double
+    }
+    
+    struct WeatherResponse: Decodable{
+        var id: Double
+        var main: String
+        var description: String
+    }
+    
+    struct MainResponse: Decodable{
+        var temp: Double
+        var feels_like: Double
+        var temp_min: Double
+        var temp_max: Double
+        var pressure: Double
+        var humidity: Double
+    }
+    
+    struct WindResponse: Decodable{
+        var speed: Double
+        var deg: Double
+    }
+}
+
+
+extension ResponseBody.MainResponse{
+    var feelsLike: Double {
+        return feels_like
+    }
+    var tempMin: Double {
+        return temp_min
+    }
 }
--- WeatherApp/WeatherApp/Views/WelcomeView.swift
+++ WeatherApp/WeatherApp/Views/WelcomeView.swift
@@ -6,13 +6,32 @@
 //
 
 import SwiftUI
+import CoreLocationUI
 
 struct WelcomeView: View {
     @EnvironmentObject var locationManager:LocationManager
     var body: some View {
         VStack{
+            VStack(spacing: 20){
+                Text("Welcome to the Weather App")
+                    .bold().font(.title)
+                
+                Text("Please share your current location to get the weather in your area")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation){
+                locationManager.requestLocation()
+            }
+            .cornerRadius(20)
+            .symbolVariant(/*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            .foregroundColor(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
+            
             
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
